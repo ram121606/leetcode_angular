140761644e3291bceae2762b0552a28a17d3c764
ram121606 <ramasubramanyam12@gmail.com>
2023-11-24 13:09:10 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import {FormsModule} from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms'
+import {MatInputModule} from '@angular/material/input'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {Validators, FormControl} from '@angular/forms'
+import {HttpClient} from '@angular/common/http'
+import {environment} from '../../environments/environment'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent{
+  username : string = '';
+  lt_name = new FormControl('',[Validators.required])
+  result : any = '';
+
+  constructor(private http: HttpClient){}
+
+  onClick(){
+    this.http.get<any>(environment.BACKEND_URL+this.username).subscribe(resp=>{
+      console.log(resp);
+      this.result = resp;
+    })
+  }
+  // onClick(){
+  //   this.http.get<any>('http://127.0.0.1:6969/'+this.username).subscribe(response=>{
+  //     return response;
+  //   })
+  // }
+
+  getErrorMessage(){
+    if(this.lt_name.hasError('required')){
+      return "Username cannot be empty"
+    }
+    return ""
+  }
+}
